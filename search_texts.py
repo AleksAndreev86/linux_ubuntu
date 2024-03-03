42b2dd531c60c26c9e32fb0668dac9b8bbaf6ef2
Александр <aleksandr.andreev.1986@yandex.ru>
2024-03-03 16:45:48 +0500

    Изменены данные вывода условия else, добавлена функция проверки состояния выполнения - номер кода вывода

--- search_texts.py
+++ search_texts.py
@@ -2,16 +2,22 @@ print('Программа поиска текста в файле')
 import subprocess
 
 def search_word(command, string):
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    result = subprocess.run('cat ' + command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
     print(True if string in result.stdout else False)
+
+def returncode_texts(command):
+    result = subprocess.run('cat ' + command, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    return result.returncode
 
 print('Введите строку текста')
 string = input('---> ')
 
-print('Введите команду и путь к файлу')
+print('Введите путь к файлу')
 command = input('---> ')
 
-if string != '' and command != '':
+if string != '' and command != '' and returncode_texts(command) == 0:
     search_word(command, string)
 else:
-    print('Одна или обе из введенных строк оказались пустыми!')
+    print('Проверьте введенные данные!')
+    
+    
